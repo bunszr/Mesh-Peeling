3104542b02189e5158ecc9f71396fa230e7ca1a2
Bünyamin SEZER <bunyaminsezere@gmail.com>
2023-08-01 18:17:54 +0300

    Added different shell as ShellControllerLittleRotation to peeling mesh almost no rotation

---
+++ Assets/Scripts/Movements/Shell/ShellControllerLittleRotation.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class ShellControllerLittleRotation : ShellControllerBase
+{
+    public Transform shellCenterT;
+    public float shellAngleSpeedAdder = 5;
+
+    protected override void Start()
+    {
+        base.Start();
+        shellMeshContainer.CurrShellMesh = shellMeshContainer.Rent();
+        shellMeshContainer.CurrShellMesh.transform.parent = null;
+        shellMeshContainer.CurrShellMesh.transform.localPosition = shellCenterT.localPosition;
+        shellMeshContainer.CurrShellMesh.transform.localRotation = shellCenterT.localRotation;
+    }
+
+    protected override void OnStartPeeling()
+    {
+        base.OnStartPeeling();
+    }
+
+    protected override void OnEndPeeling()
+    {
+        base.OnEndPeeling();
+
+        shellMeshContainer.CurrShellMesh.rb.isKinematic = false;
+
+        shellMeshContainer.Return(shellMeshContainer.CurrShellMesh);
+
+        shellMeshContainer.CurrShellMesh = shellMeshContainer.Rent();
+        shellMeshContainer.CurrShellMesh.rb.isKinematic = true;
+        shellMeshContainer.CurrShellMesh.transform.parent = null;
+        shellMeshContainer.CurrShellMesh.transform.localPosition = shellCenterT.localPosition;
+        shellMeshContainer.CurrShellMesh.transform.localRotation = shellCenterT.localRotation;
+    }
+
+    private void Update()
+    {
+        if (!hasPeeling) return;
+
+        float angle = (shellAngleSpeedAdder + Utility.GetAngleSpeedFromSpeed(knife.velocity, ShellMeshRadius) + GetAngleSpeedFromAngleSpeedOfRotater()) * Time.deltaTime;
+        shellMeshContainer.CurrShellMesh.transform.rotation *= Quaternion.AngleAxis(angle, Vector3.up);
+    }
+}
